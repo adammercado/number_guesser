0fa2adb2e8bba79d29f8b8ed223dbb732dddcdaa
Adam Merado <adam.mercado@rocketmail.com>
2021-03-27 03:52:59 -0400

    added functionality to update the score.

--- script.js
+++ script.js
@@ -3,10 +3,21 @@ let computerScore = 0;
 let currentRoundNumber = 1;
 
 // Write your code below:
+
+/*
+generateTarget() takes no inputs and returns a random integer
+between 0 and 9.
+*/
 const generateTarget = () => {
     return Math.floor(Math.random() * 10);
 }
 
+/*
+compareGuesses() takes the human guess, the computer guess and
+the target value as inputs and returns who won based off of who
+was closer to the target value. If there is a tie, the human 
+automatically wins.
+*/
 const compareGuesses = (human, computer, target) => {
     /*
     Determines which player (human or computer wins)
@@ -23,4 +34,26 @@ const compareGuesses = (human, computer, target) => {
         return false;
     }
 }
+
+/*
+updateScore() takes a string as an input representing 'human' or 
+'computer' and updates the score by 1 point based off of which player
+was passed in as an argument.
+*/
+const updateScore = winner => {
+    //if winner is human
+    if (winner === 'human') {
+        humanScore++;
+    }
+
+    //if winner is computer
+    if (winner === 'computer') {
+        computerScore++;
+    }
+
+}
+
+
+
+
 
